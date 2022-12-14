50fc039b812a91eef2ae0d4b1cc68c0ba0b37db3
Dan <76595911+DANser-freelancer@users.noreply.github.com>
2022-12-14 14:53:50 +0100

    Update shawzin compiler script.js

--- shawzin compiler script.js
+++ shawzin compiler script.js
@@ -12,14 +12,6 @@ const findNoteRegex = /\D+/i;
 const findDoubleNoteRegex = /[a-z]+[+]+[a-z]+/i;
 const findTripleNoteRegex = /[a-z]+[+]+[a-z]+[+]+[a-z]+/i;
 let database;
-fetch("./music codes database.json")
-	.then((resp) => {
-		return resp.json();
-	})
-	.then((data) => {
-		database = data;
-		console.log(database);
-	});
 copyBtn.style.disabled = true;
 const timingConvertRules = 
 ['A','B','C','D','E','F','G','H','I','J','K','L','M',
