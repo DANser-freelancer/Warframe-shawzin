b7fbbcf7bf1920627e4bac6c17758a32cee26562
Dan <76595911+DANser-freelancer@users.noreply.github.com>
2023-01-07 17:58:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
-require('dotenv').config();
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 console.log(process.env.TELEGRAM_TOKEN);
 //Global variables
 const translateBtn = document.getElementById('translate');
